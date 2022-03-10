609562b093d750402bfdede2459f518a32031b1f
Praveen Kumar <praveennick1@gmail.com>
2022-03-10 11:49:36 +0530

    added notification component

---
+++ src/Components/Notification.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../assets/css/Notification.css";
+
+function Notification() {
+  return (
+    <div className="notification">
+      <h5>hello</h5>
+    </div>
+  );
+}
+
+export default Notification;
