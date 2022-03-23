ace592840c55edc56f046f89761bab87168e1cdc
Praveen Kumar <praveennick1@gmail.com>
2022-03-23 16:22:00 +0530

    added Notification Component

--- src/Components/Notification.js
+++ src/Components/Notification.js
@@ -1,10 +1,67 @@
 import React from "react";
 import "../assets/css/Notification.css";
+import { ReactComponent as NotificationBell } from "../assets/icons/notification-bell.svg";
+import { ReactComponent as NotificationDelete } from "../assets/icons/delete-forever.svg";
+import NotificationItem from "./NotificationItem";
+import NotificationTime from "./NotificationTime";
 
-function Notification() {
+// import Box from "@mui/material/Box";
+// import Modal from "@mui/material/Modal";
+
+function Notification({ open, handleClose }) {
+  // console.log("open", open);
+  // const style = {
+  //   position: "absolute",
+  //   top: "13%",
+  //   right: "13%",
+  //   width: "23%",
+  //   // transform: "translate(-50%, -50%)",
+  //   // width: "40%",
+  //   bgcolor: "background.paper",
+  //   // border: "2px solid #000",
+  //   boxShadow: 24,
+  //   borderRadius: 1,
+  //   overFlow: "hidden",
+  //   p: 0,
+  // };
   return (
+    // <Modal open={open} onClose={handleClose}>
+    //   <Box sx={style}>
+    //     <div className="notification">
+    //       <div className="notification-header">
+    //         <NotificationBell className="notification-bell" />
+    //         <h3>Notification</h3>
+    //         <NotificationDelete className="notification-delete" />
+    //       </div>
+    //       <div className="notification-content">
+    //         <NotificationTime time={"Today"} />
+    //         <NotificationItem />
+    //         <NotificationTime time={"Yesterday"} />
+    //         <NotificationItem />
+    //         <NotificationTime time={"Older Notification"} />
+    //         <NotificationItem />
+    //         <NotificationItem />
+    //         <NotificationItem />
+    //       </div>
+    //     </div>
+    //   </Box>
+    // </Modal>
     <div className="notification">
-      <h5>hello</h5>
+      <div className="notification-header">
+        <NotificationBell className="notification-bell" />
+        <h3>Notification</h3>
+        <NotificationDelete className="notification-delete" />
+      </div>
+      <div className="notification-content">
+        <NotificationTime time={"Today"} />
+        <NotificationItem />
+        <NotificationTime time={"Yesterday"} />
+        <NotificationItem />
+        <NotificationTime time={"Older Notification"} />
+        <NotificationItem />
+        <NotificationItem />
+        <NotificationItem />
+      </div>
     </div>
   );
 }
