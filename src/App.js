71e70bb904bce9ce31673f9d869a113f6542470b
Praveen Kumar <praveennick1@gmail.com>
2022-03-23 14:25:05 +0530

    added some routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import Home from "./Components/Home";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Login from "./Components/Login";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      <Router>
+        <Route exact path="/" component={Login} />
+        <Route path="/home" component={Home} />
+      </Router>
     </div>
   );
 }
