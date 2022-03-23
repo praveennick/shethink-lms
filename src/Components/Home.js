abca954f2a213cf1fe2538b43ac032b7bdaf4bcd
Praveen Kumar <praveennick1@gmail.com>
2022-03-23 16:19:42 +0530

    added Router and routes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,45 @@
 import React from "react";
-import Login from "./Login";
+// import Login from "./Login";
 import "../assets/css/Home.css";
+
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
 import Dashboard from "./Dashboard";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Courses from "./Courses";
+import MyProfile from "./MyProfile";
+import ViewCourse from "./ViewCourse";
+import HrHome from "./HrHome";
+import Modules from "./Modules";
+import InternModel from "./InternModel";
+import ScrollToTop from "./ScrollToTop";
+import ViewModule from "./ViewModule";
+import CreateModule from "./CreateModule";
+import EditModule from "./EditModule";
 
 function Home() {
   return (
     <div className="container_wrap">
-      {/* <Login /> */}
-      <Dashboard />
+      <Router>
+        <ScrollToTop />
+        <Sidebar />
+        <Navbar />
+        <Switch>
+          <div className="home-content">
+            <Route path="/home" component={HrHome} />
+            <Route path="/courses" component={Courses} />
+            <Route path="/profile" component={MyProfile} />
+            <Route path="/viewCourse" component={ViewCourse} />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/modules" component={Modules} />
+            <Route path="/internModel" component={InternModel} />
+            <Route path="/viewModule" component={ViewModule} />
+            <Route path="/createModule" component={CreateModule} />
+            <Route path="/editModule" component={EditModule} />
+          </div>
+        </Switch>
+      </Router>
     </div>
   );
 }
