88a4ce5d26bed5988d50a4ecc6709ba8952327f7
Praveen Kumar <praveennick1@gmail.com>
2022-03-23 16:20:21 +0530

    added onchange and value and styles

--- src/Components/InputField.js
+++ src/Components/InputField.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "../assets/css/InputField.css";
 
-function InputField({ label, type, placeholder }) {
+function InputField({ label, type, placeholder, onChange, onValue, style }) {
   return (
     <>
       <label htmlFor="" className="login-label">
         {label}
       </label>
-      <input type={type} placeholder={placeholder} className="input-field" />
+      <input
+        type={type}
+        placeholder={placeholder}
+        className="input-field"
+        onChange={onChange}
+        value={onValue}
+        style={style}
+      />
     </>
   );
 }
