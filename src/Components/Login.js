128dd77d42c9637558b562fcf84c1a551d133385
Praveen Kumar <praveennick1@gmail.com>
2022-03-24 11:06:01 +0530

    added loginUser authentication with redux

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import "../assets/css/Login.css";
 
 import loginImage from "../assets/images/000.png";
@@ -6,6 +7,16 @@ import logo from "../assets/images/logo.svg";
 import InputField from "./InputField";
 
 function Login() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const userSignin = useSelector((state) => state.userSignin);
+
+  const loginUser = (event) => {
+    event.preventDefault();
+    console.log(email, password);
+  };
+
   return (
     <div className="login-container">
       <div className="login-left">
@@ -20,7 +31,7 @@ function Login() {
       </div>
       <div className="login-right">
         <img src={logo} alt="" className="logo" />
-        <div className="login-content">
+        <form className="login-content">
           <h1 className="welcome-heading">welcome🖐</h1>
           <p className="login-welcome">
             Welcome User, Please Enter Your Valid Details.
@@ -29,13 +40,21 @@ function Login() {
             label="Email"
             type="text"
             placeholder="example@gmail.com"
+            onChange={(e) => setEmail(e.target.value)}
           />
-          <InputField label="Password" type="password" placeholder="*******" />
+          <InputField
+            label="Password"
+            type="password"
+            placeholder="*******"
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <a href="www.google.com" className="forget-pwd">
             Forget Password
           </a>
-          <button className="login-btn">Sign In</button>
-        </div>
+          <button className="login-btn" onClick={loginUser}>
+            Sign In
+          </button>
+        </form>
       </div>
     </div>
   );
