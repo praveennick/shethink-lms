37a264fcb046237de304769962ed0b559f1e89f4
Praveen Kumar <praveennick1@gmail.com>
2022-03-24 11:06:40 +0530

    added initialState, combineReducers and store

---
+++ src/redux/store.js
@@ -0,0 +1,20 @@
+import { createStore, applyMiddleware, combineReducers, compose } from "redux";
+import thunk from "redux-thunk";
+import { userReducer } from "./reducers/user.reducer";
+
+const initialState = {
+  userSignin: {},
+};
+
+const reducers = combineReducers({
+  userSignin: userReducer,
+});
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducers,
+  initialState,
+  composeEnhancer(applyMiddleware(thunk))
+);
+export default store;
