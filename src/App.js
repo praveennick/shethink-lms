33ddd77c7c69ff3ba4a635fe594cef7502109e21
Praveen Kumar <praveennick1@gmail.com>
2022-03-25 18:40:20 +0530

    added routers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
 import "./App.css";
-import Home from "./Components/Home";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./assets/css/Home.css";
+
+import Navbar from "./Components/Navbar";
+import Sidebar from "./Components/Sidebar";
+import Dashboard from "./Components/Dashboard";
 import Login from "./Components/Login";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Courses from "./Components/Courses";
+import MyProfile from "./Components/MyProfile";
+import ViewCourse from "./Components/ViewCourse";
+import HrHome from "./Components/HrHome";
+import Modules from "./Components/Modules";
+import InternModel from "./Components/InternModel";
+import ScrollToTop from "./Components/ScrollToTop";
+import ViewModule from "./Components/ViewModule";
+import CreateModule from "./Components/CreateModule";
+import EditModule from "./Components/EditModule";
+import Dummy from "./Components/Dummy";
 
 function App() {
   return (
-    <div className="App">
-      {/* <Home /> */}
+    <div className="App container_wrap">
       <Router>
         <Switch>
           <Route exact path="/" component={Login} />
-          <Route path="/home" component={Home} />
+          <Route exact path="/dummy" component={Dummy} />
+          <div>
+            <ScrollToTop />
+            <Sidebar />
+            <Navbar />
+            <div className="home-content">
+              <Route exact path="/home" component={HrHome} />
+              <Route exact path="/courses" component={Courses} />
+              <Route exact path="/profile" component={MyProfile} />
+              <Route exact path="/viewCourse" component={ViewCourse} />
+              <Route exact path="/dashboard" component={Dashboard} />
+              <Route exact path="/modules" component={Modules} />
+              <Route exact path="/internModel" component={InternModel} />
+              <Route exact path="/viewModule" component={ViewModule} />
+              <Route exact path="/createModule" component={CreateModule} />
+              <Route exact path="/editModule" component={EditModule} />
+            </div>
+          </div>
         </Switch>
       </Router>
     </div>
