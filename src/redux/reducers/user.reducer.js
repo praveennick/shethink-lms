267650a0ff8974ec6b0c268b4a95e9beb96d25ee
Praveen Kumar <praveennick1@gmail.com>
2022-03-25 18:44:02 +0530

    some name changes

--- src/redux/reducers/user.reducer.js
+++ src/redux/reducers/user.reducer.js
@@ -5,14 +5,14 @@ import {
 } from "../../constants";
 
 export const userReducer = (
-  currentState = { loading: true, payload: {} },
+  currentState = { loading: true, userInfo: null },
   action
 ) => {
   switch (action.type) {
     case USER_SIGNIN_REQUEST:
       return { loading: true };
     case USER_SIGNIN_SUCCESS:
-      return { loading: false, payload: action.payload };
+      return { loading: false, userInfo: action.userInfo };
     case USER_SIGNIN_FAILURE:
       return { loading: false, error: action.error };
     default:
