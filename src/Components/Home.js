a519337f8bfaee40d34fe18430c52dfcd21e56b3
Praveen Kumar <praveennick1@gmail.com>
2022-03-25 18:40:47 +0530

    commented home component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,48 +1,47 @@
-import React from "react";
+// import React from "react";
 // import Login from "./Login";
-import "../assets/css/Home.css";
+// import "../assets/css/Home.css";
 
-import Navbar from "./Navbar";
-import Sidebar from "./Sidebar";
-import Dashboard from "./Dashboard";
+// import Navbar from "./Navbar";
+// import Sidebar from "./Sidebar";
+// import Dashboard from "./Dashboard";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Courses from "./Courses";
-import MyProfile from "./MyProfile";
-import ViewCourse from "./ViewCourse";
-import HrHome from "./HrHome";
-import Modules from "./Modules";
-import InternModel from "./InternModel";
-import ScrollToTop from "./ScrollToTop";
-import ViewModule from "./ViewModule";
-import CreateModule from "./CreateModule";
-import EditModule from "./EditModule";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import Courses from "./Courses";
+// import MyProfile from "./MyProfile";
+// import ViewCourse from "./ViewCourse";
+// import HrHome from "./HrHome";
+// import Modules from "./Modules";
+// import InternModel from "./InternModel";
+// import ScrollToTop from "./ScrollToTop";
+// import ViewModule from "./ViewModule";
+// import CreateModule from "./CreateModule";
+// import EditModule from "./EditModule";
 
-function Home() {
-  return (
-    <div className="container_wrap">
-      <Router>
-        <ScrollToTop />
-        <Sidebar />
-        <Navbar />
+// function Home() {
+//   return (
+//     <div className="container_wrap">
+//       <Router>
+//         <ScrollToTop />
+//         <Sidebar />
+//         <Navbar />
+//         <div className="home-content">
+//           <Switch>
+//             <Route exact path="/home" component={HrHome} />
+//             <Route path="/courses" component={Courses} />
+//             <Route path="/profile" component={MyProfile} />
+//             <Route path="/viewCourse" component={ViewCourse} />
+//             <Route path="/dashboard" component={Dashboard} />
+//             <Route path="/modules" component={Modules} />
+//             <Route path="/internModel" component={InternModel} />
+//             <Route path="/viewModule" component={ViewModule} />
+//             <Route path="/createModule" component={CreateModule} />
+//             <Route path="/editModule" component={EditModule} />
+//           </Switch>
+//         </div>
+//       </Router>
+//     </div>
+//   );
+// }
 
-        <div className="home-content">
-          <Switch>
-            <Route path="/home" component={HrHome} />
-            <Route path="/courses" component={Courses} />
-            <Route path="/profile" component={MyProfile} />
-            <Route path="/viewCourse" component={ViewCourse} />
-            <Route path="/dashboard" component={Dashboard} />
-            <Route path="/modules" component={Modules} />
-            <Route path="/internModel" component={InternModel} />
-            <Route path="/viewModule" component={ViewModule} />
-            <Route path="/createModule" component={CreateModule} />
-            <Route path="/editModule" component={EditModule} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
-  );
-}
-
-export default Home;
+// export default Home;
