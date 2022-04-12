96fce30336cd9ebb885f533cd6e77fee199510f1
Praveen Kumar <praveennick1@gmail.com>
2022-04-12 18:30:23 +0530

    some changes

--- src/Components/CourseItem.js
+++ src/Components/CourseItem.js
@@ -1,29 +1,30 @@
 import React from "react";
 import "../assets/css/CourseItem.css";
 import { ReactComponent as LinkedinLogo } from "../assets/icons/linkedin-square.svg";
-import moment from 'moment';
+import moment from "moment";
 
 import { Link } from "react-router-dom";
 
-function CourseItem({data}) {
+function CourseItem({ data }) {
+  console.log(data.courseID, "courseID");
+  localStorage.setItem("courseID", data.courseID);
   return (
     <div className="courseItem">
       <div className="courseItem-upper">
         <div className="upper-left">
           <h3 className="courseItem-heading">{data.courseName}</h3>
-          <p>Creation Date - {moment(data.createdAt).format("d/MMM/Y")}
-          </p>
+          <p>Creation Date - {moment(data.createdAt).format("d/MMM/Y")}</p>
         </div>
         <div className="upper-right">
           <h4>Created by-{data.author}</h4>
           <a href="www.google.com" className="coursesItem-linkedin">
             Follow <LinkedinLogo style={{ marginLeft: "5px" }} />
           </a>
-        </div>  
+        </div>
       </div>
       <div className="courseItem-lower">
         <p className="courseItem-disc">discription- {data.courseDescription}</p>
-        <Link className="viewCourse-btn" to="/viewCourse">
+        <Link className="viewCourse-btn" to="/viewCourse" hello={"hello"}>
           View Course
         </Link>
       </div>
